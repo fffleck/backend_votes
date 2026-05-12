ce2aa9555328675627c7b37ea5be7ace7174c7a7
Fabio Fleck <fabio.fleck@besolution.com.br>
2026-05-12 14:56:29 -0300

    healtcheck api

--- src/server.ts
+++ src/server.ts
@@ -6,3 +6,10 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`)
 })
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "online",
+    message: "API funcionando"
+  });
+});
